1e6df3546f604c0710063037b761fd089843a74f
Jobson Alves <jammeloalves@gmail.com>
2021-06-13 15:29:11 -0300

    config beckend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import Button from '@material-ui/core/Button';
 function App() {
   return (
     <div>
      <h1>Sistema PVC</h1>
+     <Button variant="contained" color="primary">
+      Hello World
+    </Button>
     </div>
   );
 }
